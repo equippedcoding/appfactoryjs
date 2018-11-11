8b1bb35facdd7bd8de587c3d6d6f403608aafad4
james mitchell <jamesmitchell@jamess-MacBook-Pro.local>
2018-11-11 11:43:37 -0600

    initial commit

--- config.appfac.js
+++ config.appfac.js
@@ -1,6 +1,6 @@
 {
 	"index-config": {
-		"title":"App Title",
+		"title":"",  
 		"doctype":"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">",
 		"meta": [
 			"<meta charset=\"UTF-8\">",
